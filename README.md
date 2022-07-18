75abc2308db2bb43c2753ebf74f515791e5f1edb
utkarsh07-co-in <utkarsh7000@outlook.com>
2022-07-18 19:46:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-92w1jy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-92w1jy)
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello Meme Team!!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
